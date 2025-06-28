f2a83c2fa27d1227d0852c8c169c9af777ccd8f6
Tegar Subagdja <kingtegar1510@gmail.com>
2025-06-28 12:32:54 +0700

    better findcontour in z_mapping.py

--- Z_Mapping.py
+++ Z_Mapping.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('Image/2.jpg', 0)
+image = cv2.imread('Image/8.jpg')
 image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-_, thresh = cv2.threshold(image, 50, 255, cv2.THRESH_BINARY_INV)
+_, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)
 kernel = np.ones((3, 3), np.uint8)
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
 thresh = cv2.erode(thresh, kernel, iterations=3)
@@ -14,14 +15,22 @@ contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPL
 for cnt in contours:
     area = cv2.contourArea(cnt)
     if area > 3000:
-        epsilon = 0.2 * cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, epsilon, True)
         
-        cv2.drawContours(image, [approx], -1, (255, 255, 255), 2)
-        for point in approx:
-            x, y = point.ravel()
-            cv2.circle(image, (int(x), int(y)), 5, (255, 255, 255), -1)
+        rect = cv2.minAreaRect(cnt)
+        box = cv2.boxPoints(rect)
+        box = box.astype(int)
 
-cv2.imshow('Hasil', image)
+        # Gambar kotak luar
+        cv2.polylines(image, [box], True, (0, 255, 255), 2)
+
+        # Gambar dua diagonal silang
+        cv2.line(image, tuple(box[0]), tuple(box[2]), (255, 0, 255), 2)  # Diagonal 1
+        cv2.line(image, tuple(box[1]), tuple(box[3]), (0, 255, 0), 2)    # Diagonal 2
+
+        # Tandai sudut
+        for pt in box:
+            cv2.circle(image, tuple(pt), 4, (0, 0, 255), -1)
+
+cv2.imshow('Diagonal Silang', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
