0f4e29af4d36eb3e2949128aa50996cfbe021613
Tegar Subagdja <kingtegar1510@gmail.com>
2025-06-28 11:53:32 +0700

    get diagonal corner of the box properly

--- Z_Mapping.py
+++ Z_Mapping.py
@@ -1,21 +1,27 @@
-from Utils import *
 import cv2
+import numpy as np
 
-image = cv2.imread('1-getPath.jpg', 0)
+image = cv2.imread('Image/2.jpg', 0)
+image = cv2.resize(image, (0,0), fx=0.3, fy=0.3)
 
-cv2.namedWindow('Threshold', cv2.WINDOW_NORMAL)
+_, thresh = cv2.threshold(image, 50, 255, cv2.THRESH_BINARY_INV)
+kernel = np.ones((3, 3), np.uint8)
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+thresh = cv2.erode(thresh, kernel, iterations=3)
 
-for i in range(1, 255):
-    _, thresh = cv2.threshold(image, i, 255, cv2.THRESH_BINARY_INV)
-    
-    # Salin gambar biar aslinya tidak berubah
-    display = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)  # Ubah ke BGR agar bisa diberi warna teks
-    
-    # Tambahkan teks di pojok kiri atas
-    cv2.putText(display, f'Threshold: {i}', (10, 30), 
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    
-    cv2.imshow('Threshold', display)
-    cv2.waitKey(100)  # Atur delay
+contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+for cnt in contours:
+    area = cv2.contourArea(cnt)
+    if area > 3000:
+        epsilon = 0.2 * cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, epsilon, True)
+        
+        cv2.drawContours(image, [approx], -1, (255, 255, 255), 2)
+        for point in approx:
+            x, y = point.ravel()
+            cv2.circle(image, (int(x), int(y)), 5, (255, 255, 255), -1)
+
+cv2.imshow('Hasil', image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
