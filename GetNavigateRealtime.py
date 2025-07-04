fe0e8a3283f7c5a09bb367d820426ffc8451bee4
Tegar Subagdja <kingtegar1510@gmail.com>
2025-07-04 22:55:21 +0700

    fix camera orientation on GetNavigationRealtime

--- GetNavigateRealtime.py
+++ GetNavigateRealtime.py
@@ -1,7 +1,7 @@
 from Utils import *
 import cv2
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(1)
 target_point = (100, 100)
 
 if not cap.isOpened():
@@ -15,19 +15,20 @@ while True:
         break
 
     # Proses deteksi tetap pada frame asli
-    result = GetOrientation(cam, id=0, target_point=target_point, show_result=False)
+    result = GetOrientation(cam, id=9, target_point=target_point, show_result=False)
 
     if result and result['error_orientasi_derajat'] is not None:
-        frame = result['image']
+        frame = cv2.flip(result['image'], 1)
         center = result['koordinat']
         error = f"{int(result['error_orientasi_derajat'])}"
         distance = f"{int(result['distance'])}"
-        cv2.circle(cam, target_point, 10, (255, 128, 255), -1)
-        cv2.putText(cam, error, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
-        cv2.putText(cam, distance, (40, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
-        cv2.imshow('Realtime ArUco Tracking', cam)
+        cv2.circle(frame, target_point, 10, (255, 128, 255), -1)
+        cv2.putText(frame, error, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+        cv2.putText(frame, distance, (40, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+        cv2.imshow('Realtime ArUco Tracking True', frame)
     else:
-        cv2.imshow('Realtime ArUco Tracking', cam)
+        cam = cv2.flip(cam, 1)
+        cv2.imshow('Realtime ArUco Tracking True', cam)
 
     # Tekan tombol ESC (27) untuk keluar
     if cv2.waitKey(1) & 0xFF == 27:
