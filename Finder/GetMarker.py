1be804d2f85b7f1b6d8392d28c6349c14b6212b9
Tegar Subagdja <kingtegar1510@gmail.com>
2025-07-22 04:35:37 +0700

    perapihan awal

--- Finder/GetMarker.py
+++ Finder/GetMarker.py
@@ -9,43 +9,52 @@ def Pos(img):
     detector = aruco.ArucoDetector(detector_dict, detector_params)
     corners, ids, _ = detector.detectMarkers(img)
 
-    # Variabel untuk start dan goal
+    # Variabel untuk meyimpan start dan goal
     start = None
     goal = None
 
+    # Jika ada marker ditemukan (adanya ids)
     if ids is not None:
-        # Mencari Corner
         ids = ids.flatten()
         for i, marker_id in enumerate(ids):
             if marker_id == 3 or marker_id == 1:
+                # Dapatkan corcer start
                 pts = corners[i][0]
+                # Dapatkan center start
                 center = np.mean(pts, axis=0).astype(int)
+                # Simpan posisi start
                 start = (center, pts)
             elif marker_id == 9 or marker_id == 7:
+                # Dapatkan corcer goal
                 pts = corners[i][0]
+                # Dapatkan center goal
                 center = np.mean(pts, axis=0).astype(int)
+                # Simpan posisi start
                 goal = (center, pts)
+                # Hitung Lebar Marker Goal
                 mark_size = 0.5 * (np.linalg.norm(pts[0] - pts[1]) + np.linalg.norm(pts[2] - pts[3]))
 
+        # Gambar informasi Marker
         aruco.drawDetectedMarkers(image=img, corners=corners)
 
         if not start or not goal:
             return None, None, None
 
+        # Ubah ke dalam bentuk tupple
         startPts = [(int(x), int(y)) for x, y in start[1]]
+        tStart = tuple(int(x) for x in start[0])
+        start = (tStart, startPts)
+        # Ubah kedalam bentuk tupple
         goalPts = [(int(x), int(y)) for x, y in goal[1]]
-
-        s = tuple(int(x) for x in start[0])
-        g = tuple(int(x) for x in goal[0])
-
-        start = (s, startPts)
-        goal = (g, goalPts)
+        tGoal = tuple(int(x) for x in goal[0])
+        goal = (tGoal, goalPts)
 
         return (
             start,
             goal,
             mark_size
         )
+    
     else:
         return 0,0,0
     
--- Finder/GetPrep.py
+++ Finder/GetPrep.py
@@ -3,51 +3,48 @@ from Utils import *
 scale=20
 
 def Prep(img, start, goal, markSize):
-    # Step 1: Grayscale
+    # Grayscale
     if len(img.shape) == 3:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Step 2: Thresholding
+    # Thresholding
     _, binary = cv2.threshold(img, 60, 255, cv2.THRESH_BINARY_INV)
 
     kernel = np.ones((9,9), np.uint8)
     binary = cv2.erode(binary, kernel=kernel, iterations=3)
 
-    # Step 3: Distance Transform
+    # Distance Transform
     dist = cv2.distanceTransform(255 - binary, cv2.DIST_L2, 5)
     buffer_radius = int(3 * markSize)
     buffered_obstacle = np.uint8(dist < buffer_radius) * 255
 
-    # Step 4: Tambahkan buffer ke binary → hasil baru
+    # Tambahkan buffer ke binary → hasil baru
     binary_with_buffer = cv2.bitwise_or(binary, buffered_obstacle)
 
-    # Step 5: Timpa robot dengan warna hitam
+    # Timpa robot dengan warna hitam
     if start is not None and goal is not None:
         cv2.circle(binary_with_buffer, goal[0], int(2*markSize), 0, -1)
         cv2.circle(binary_with_buffer, start[0], int(2*markSize), 0, -1)
 
+    # Downsampling
     x, y = (1/scale), (1/scale)
     resize = cv2.resize(binary_with_buffer, (0,0), fx=x, fy=y)
-
     _, resize = cv2.threshold(resize, 100, 255, cv2.THRESH_BINARY)
-
-    print(np.unique(resize))
-
-    cv2.imwrite('Hasil Prep.jpg', resize)
+    # cv2.imwrite('Hasil Prep.jpg', resize)
 
     return resize
 
 def PrepCoord(start, goal, path=None):
 
     if path is None:
-        # Hanya ubah start dan goal (misal kalikan scale)
+        # Ubah skala start dan goal ke ukuran kecil
         x, y = start[0]
         pStart = (y // scale, x // scale)
         x, y = goal[0]
         pGoal = (y // scale, x // scale)
         return pStart, pGoal
     else:
-        # Ubah start, goal, dan path (misal dibagi scale)
+        # Ubah Skala start, goal, dan path  ke ukuran semula
         x, y = path[0]
         pStart = (y * scale, x * scale)
         x, y = path[-1]
